c0e9d5f6fe0e9ed18425a32ac0dd5104f4d83099
Vagan Mkrtchyan <v.mkrtchyan@i-sys.ru>
2020-08-16 14:20:47 +0400

    Updates

---
+++ precache-manifest.635389c777536eca70d62a8d4d1372b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6bb9a77212e0c18a9cda266b5f0c88",
+    "url": "/catalog/index.html"
+  },
+  {
+    "revision": "c710c15ea23d3190ddf8",
+    "url": "/catalog/static/css/main.580fc16a.chunk.css"
+  },
+  {
+    "revision": "c9db9c003ef81a782bc3",
+    "url": "/catalog/static/js/2.a940b15b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/catalog/static/js/2.a940b15b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c710c15ea23d3190ddf8",
+    "url": "/catalog/static/js/main.b73b69ab.chunk.js"
+  },
+  {
+    "revision": "0c1fe8d64cf0fdcba95e",
+    "url": "/catalog/static/js/runtime-main.5a0bd241.js"
+  }
+]);
