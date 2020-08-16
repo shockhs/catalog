e1213c63c275acd9565aea8ece8973c153c546cf
Vagan Mkrtchyan <v.mkrtchyan@i-sys.ru>
2020-08-16 14:18:12 +0400

    working version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,155 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="images-container">
+        <div className="imageField">
+          <div>
+            <img
+              style={{ width: 126 }}
+              src="https://i.ibb.co/8BxzF90/1.jpg"
+              alt="1"
+            />
+            <img
+              style={{ width: 126 }}
+              src="https://i.ibb.co/XZ6wHFv/2.jpg"
+              alt="2"
+            />
+            <img
+              style={{ width: 126 }}
+              src="https://i.ibb.co/vBk6cCz/3.jpg"
+              alt="3"
+            />
+          </div>
+          <div className="numbers">
+            <h2>#1</h2>
+            <h2>#2</h2>
+            <h2>#3</h2>
+          </div>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/XXrp7VJ/4.jpg" alt="4" />
+          <h2>#4</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/4WVpZLB/5.jpg" alt="5" />
+          <h2>#5</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/9vzcYyc/6.jpg" alt="6" />
+          <h2>#6</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/sbyHmWV/7.jpg" alt="7" />
+          <h2>#7</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/vL6LSQQ/8.jpg" alt="8" />
+          <h2>#8</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/z8q8wLW/9.jpg" alt="9" />
+          <h2>#9</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/mBBsCqq/10.jpg" alt="10" />
+          <h2>#10</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/Y7DH5fT/11.jpg" alt="11" />
+          <h2>#11</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/MkD8SYy/12.jpg" alt="12" />
+          <h2>#12</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/ZBFQ1C7/13.jpg" alt="13" />
+          <h2>#13</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/gyyy8HQ/14.jpg" alt="14" />
+          <h2>#14</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/tMKPZry/15.jpg" alt="15" />
+          <h2>#15</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/HKZ6Thq/16.jpg" alt="16" />
+          <h2>#16</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/yBfrqyw/17.jpg" alt="17" />
+          <h2>#17</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/D9s7S6D/18.jpg" alt="18" />
+          <h2>#18</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/hZB7NWZ/19.jpg" alt="19" />
+          <h2>#19</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/3SXMyk7/20.jpg" alt="20" />
+          <h2>#20</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/D9D22gp/21.jpg" alt="21" />
+          <h2>#21</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/SNpWnyJ/22.jpg" alt="22" />
+          <h2>#22</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/0hrCq6x/23.jpg" alt="23" />
+          <h2>#23</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/XjKymWK/24.jpg" alt="24" />
+          <h2>#24</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/vDHkm6C/25.jpg" alt="25" />
+          <h2>#25</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/WsTYLtL/26.jpg" alt="26" />
+          <h2>#26</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/vwV54Jm/27.jpg" alt="27" />
+          <h2>#27</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/zbXdF1k/28.jpg" alt="28" />
+          <h2>#28</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/0cW7Z6N/29.jpg" alt="29" />
+          <h2>#29</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/p2wYLrc/30.jpg" alt="30" />
+          <h2>#30</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/MZDMjJ4/31.jpg" alt="31" />
+          <h2>#31</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/6NbyWtS/32.jpg" alt="32" />
+          <h2>#32</h2>
+        </div>
+        <div className="imageField">
+          <img src="https://i.ibb.co/yPp8c1g/33.jpg" alt="33" />
+          <h2>#33</h2>
+        </div>
+      </div>
     </div>
   );
 }
